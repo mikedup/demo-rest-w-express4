354c697dfd15be1e2961072badb30ada0e768f43
Dave Stanton <davestanton.us@gmail.com>
2014-06-14 11:51:02 -0400

    create bears.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app	= express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('demo:demo@ds033037.mongolab.com:33037/build-a-restful-api-using-node-and-express-4'); // MongoLab
+var Bear = require('./app/models/bear');
 
 // config the express4 app
 app.use(bodyParser());
@@ -15,11 +16,32 @@ var port = process.env.PORT || 8080;
 // ======================
 var router = express.Router();
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+  console.log('Something is happening.');
+  next();
+})
+
 router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });
 });
 
-// more routes needed
+router.route('/bears')
+
+  // create a bear
+  .post(function(req, res) {
+    
+    var bear = new Bear();
+    bear.name = req.body.name;
+  
+    bear.save(function(err) {
+      if (err)
+        res.send(err);
+
+      res.json({ message: 'Bear created.' });
+    });
+
+  });
 
 // register routes
 app.use('/api', router);
